4ad5fb588dc5f94fa8d3f010658477fac1c8be72
Stanley enumah <enumah44@gmail.com>
2026-07-14 14:20:55 +0100

    5501

--- config/settings.py
+++ config/settings.py
@@ -148,6 +148,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5500',
     'http://localhost:5500',
-    'http://localhost:5173'
+    'http://127.0.0.1:5501',
+    'http://localhost:5501',
+    'http://localhost:5173',
+    'http://127.0.0.1:5501',
     
 ]
