40655c0796f6cc655ad790e8e874e2c7bc146ab6
Stanley enumah <enumah44@gmail.com>
2026-07-17 10:59:43 +0100

    added production links

--- config/settings.py
+++ config/settings.py
@@ -146,11 +146,14 @@ STATICFILES_STORAGE = (
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:5500',
-    'http://localhost:5500',
-    'http://127.0.0.1:5501',
-    'http://localhost:5501',
-    'http://localhost:5173',
-    'http://127.0.0.1:5501',
-    
+    # Local development
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+    "http://127.0.0.1:5501",
+    "http://localhost:5501",
+    "http://localhost:5173",
+
+    # Production
+    "https://blog-app-bay-psi.vercel.app",
+    "https://blog-d19u.vercel.app",
 ]
